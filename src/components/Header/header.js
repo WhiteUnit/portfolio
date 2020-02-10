6203a715b9a385f0095b7a7ed423d28c145f9ef6
White Front-end <filip.lasica.3d@gmail.com>
2020-02-10 14:45:48 +0100

    Showing navbar

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,6 +1,5 @@
 import React from "react"
 import styled from "styled-components"
-import LogoSVG from "../../../assets/icons/logo.svg"
 import Nav from "./nav"
 import Container from "../container"
 
@@ -12,14 +11,10 @@ const StyledHeader = styled.header`
   align-items: center;
 `;
 
-const StyledLogo = styled.img`
-  height: 80px;
-`;
 
 const Header = () => (
   <Container>
     <StyledHeader>
-      <StyledLogo src={LogoSVG}/>
       <Nav/>
     </StyledHeader>
   </Container>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
 import Container from "../components/container"
+import Header from "../components/Header/header"
 
 export default () => (
     <Container>
+    <Header/>
     <div>Hello world!</div>
     </Container>
 )
