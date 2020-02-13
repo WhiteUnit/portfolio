74ec37f24a51552e96e39d34e394b203cad76288
White Front-end <filip.lasica.3d@gmail.com>
2020-02-13 17:54:26 +0100

    Amination on image with text

--- src/components/Skills-grid/grid-insider.js
+++ src/components/Skills-grid/grid-insider.js
@@ -2,17 +2,51 @@ import React from "react"
 import styled from "styled-components"
 import HtmlImage from "../../images/html.png"
 
-console.log(HtmlImage);
+const htmlText = "znam to czy tamto"
 
 const StyledGridInsider = styled.div`
+    position: relative;
     background-image: url(${props => props.img});
     background-size: 100%;
     width: 100%;
-    higth: 100%;
+    height: auto;
+    background-size: 100%;
+    background-position: center;
+    transition: 0.2s;
+    text-align: center;
+    font-family: lato;
+    &:hover{
+        background-size:110%;
+    }
+
+    &:after{
+        position: absolute;
+        top: 100%;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        word-wrap: break-word;
+        owerflow: hidden;
+        background-color: rgba(255,255,255,0.6);
+        color: #000;
+        padding: 8px;
+        opacity: 0;
+        content: attr(data-text) " ";
+        transition:0.4s;
+    }
+
+    &:hover:after{
+        opacity: 1;
+        top: 0;
+        padding: 8px;
+    }
 `
 
 const GridInsider = () => (
-    <StyledGridInsider img={HtmlImage}></StyledGridInsider>
+    <StyledGridInsider 
+    img={HtmlImage}
+    data-text={htmlText}
+    ></StyledGridInsider>
 )
 
 export default GridInsider;
